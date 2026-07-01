834a90ea0d40a698777f02da1db276e210a9f10a
variouslamp <sebsal2024@gmail.com>
2026-06-30 22:29:22 -0500

    add(leetcode/006): Add new challenge about sum of linked list, not finished in Rust yet

---
+++ leetcode/006/add-two-numbers.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def recolector(self, nodo):
+        lista_comp = []
+        if nodo.next:
+            lista_comp = self.recolector(nodo.next)
+            lista_comp.append(str(nodo.val))
+            return lista_comp
+        lista_comp.append(str(nodo.val))
+        return lista_comp
+
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        operaciones = [l1, l2]
+        valores = []
+        salida = []
+        lista_enlazada = []
+        for operacion in operaciones:
+            lista = self.recolector(operacion)
+            numeros = "".join(lista)
+            valores.append(int(numeros))
+        output = sum(valores)
+        for numero in str(output):
+            salida.append(int(numero))
+        adelante = None
+        for i in salida:
+            if adelante:
+                objeto = ListNode(i, adelante)
+            else:
+                objeto = ListNode(i)
+            adelante = objeto
+            lista_enlazada.insert(0, objeto)
+        return lista_enlazada[0]
+
+
+l1 = ListNode(2, ListNode(4, ListNode(3)))
+l2 = ListNode(5, ListNode(6, ListNode(4)))
+hola = Solution()
+lista = hola.addTwoNumbers(l1, l2)
+#print(lista.val, lista.next)
+print("----")
+
+for i in lista:
+    print(i.val, i.next)
---
+++ leetcode/006/solution_notes.md
@@ -0,0 +1,20 @@
+# Notas de solución
+
+---
+
+## solución 
+
+**Fecha: 2026-06-30**
+
+Durante la crecion de este problema me vi bastante acorralado la verdad ya que yo no tenia experiencia tratando con el manejo de listas enlazadas de una manera como lo solicita este problema, que es tomar los datos de las listas voltearlos y sumarlo y retornar un valor en el mismo formato que se nos entrego, mi estructura mental al leer el problema y lo que requeria fue la siguiente:
+
+1- obtener los numeros de cada una de las listas enlazadas
+2- rotarlos para que queden en el valor real y no el "espejo"
+3- Juntar los numeros y convertirlos en uno que pueda sumarte a el que se dio en la otra lista
+4- con el resultado de la suma separamos los numeros
+5- giramos los numeros como "espejo" de nuevo
+6- creamos la lista enlazada con la estructura ya definida
+
+La creacion fue interesante me obligo a usar varios metodos para recorrer la lista tranformas los datos y reconstruirlos a su forma inicial.
+
+He leido y en las propias soluciones de leetcode se muestran maneras mas eficientes haciendo uso de una tecnica llamada "carry" que es una suma de los nodos en si e ir creandolos mientras se hacen las operaciones, la solucion en RUST sera hecha de esa manera mas eficiente, puede que tambien la haga en python.
